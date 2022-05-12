3a52465f904f1f51b2d7f429a997dcf3b287c966
unknown <msandovalc23@gmail.com>
2022-05-12 12:29:05 -0500

    Agrupando archivos de ejemplos con objetos

---
+++ weekly_mission_2/examples/1_Objetos/examples0-3.js
@@ -0,0 +1,12 @@
+// Creación objeto con diferentes propiedades
+
+const auto = {
+    brand: "Ford",
+    model: "Mustang",
+    year: "1971",
+    specs:{
+        motor: "v8",
+        seats: 4,
+        wheels: 5
+    }
+}
---
+++ weekly_mission_2/examples/1_Objetos/examples0-4.js
@@ -0,0 +1,7 @@
+const mario = {
+    name: "Mario",
+    myName : function(){
+        console.log(`Hola mi nombre es ${this.name} y estoy aprendiendo a usar objetos`)
+    }
+};
+mario.myName();
