b76d7363b47a6cb313dfeae98b1b9371b2cc18f6
unknown <msandovalc23@gmail.com>
2022-05-12 12:25:06 -0500

    agregando ejemplo numero 5 de Objetos

--- weekly_mission_2/examples/examples0-5.js
+++ weekly_mission_2/examples/examples0-5.js
@@ -1,3 +1,8 @@
-const miobjeto ={
-    
-}
+const coche ={
+    name: 'Tsuru 3',
+    nickName: function(nick){
+        console.log(`A mi ${this.name} le dicen ${nick}`)
+    }
+};
+
+coche.nickName('Run Run');
